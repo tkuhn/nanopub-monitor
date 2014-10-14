0c501381854af6099f89ecdf39ebf3b61843a282
Tobias Kuhn <kuhntobias@gmail.com>
2014-10-14 16:37:25 +0200

    Add Google map

--- src/main/java/ch/tkuhn/nanopub/monitor/MonitorPage.java
+++ src/main/java/ch/tkuhn/nanopub/monitor/MonitorPage.java
@@ -1,7 +1,12 @@
 package ch.tkuhn.nanopub.monitor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.wicketstuff.gmap.GMap;
+import org.wicketstuff.gmap.api.GLatLng;
 
 
 public class MonitorPage extends WebPage {
@@ -10,6 +15,16 @@ public class MonitorPage extends WebPage {
 
 	public MonitorPage(final PageParameters parameters) {
 		super(parameters);
+		GMap map = new GMap("map");
+        map.setStreetViewControlEnabled(false);
+        map.setScaleControlEnabled(true);
+        map.setScrollWheelZoomEnabled(true);
+        List<GLatLng> points = new ArrayList<GLatLng>();
+        points.add(new GLatLng(47.3667, 8.55));
+        points.add(new GLatLng(45.4127, -75.6742));
+        points.add(new GLatLng(52.35, 4.9167));
+        map.fitMarkers(points, true);
+        add(map);
     }
 
 }
