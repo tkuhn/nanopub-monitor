85e122b11f3b000b7ed534c4569270a4fe53f77f
Tobias Kuhn <kuhntobias@gmail.com>
2021-12-27 14:51:08 +0100

    Solve CSP header problem

--- src/main/java/ch/tkuhn/nanopub/monitor/MonitorApplication.java
+++ src/main/java/ch/tkuhn/nanopub/monitor/MonitorApplication.java
@@ -1,5 +1,7 @@
 package ch.tkuhn.nanopub.monitor;
 
+import org.apache.wicket.csp.CSPDirective;
+import org.apache.wicket.csp.CSPDirectiveSrcValue;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.util.file.Folder;
@@ -17,6 +19,7 @@ public class MonitorApplication extends WebApplication {
 		super.init();
 		uploadFolder = new Folder(System.getProperty("java.io.tmpdir"), "wicket-uploads");
 		uploadFolder.mkdirs();
+		getCspSettings().blocking().disabled();
 	}
 
 	public Folder getUploadFolder() {
