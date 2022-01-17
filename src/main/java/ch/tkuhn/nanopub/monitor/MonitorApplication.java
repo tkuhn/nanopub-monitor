af1d1ffa0ce55a9264baba5e9734ec53a3c1f24a
Tobias Kuhn <kuhntobias@gmail.com>
2022-01-17 09:36:16 +0100

    Remove unnecessary upload folder code

--- src/main/java/ch/tkuhn/nanopub/monitor/MonitorApplication.java
+++ src/main/java/ch/tkuhn/nanopub/monitor/MonitorApplication.java
@@ -2,11 +2,8 @@ package ch.tkuhn.nanopub.monitor;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.util.file.Folder;
 
 public class MonitorApplication extends WebApplication {
-
-	private Folder uploadFolder = null;
 
 	@Override
 	public Class<? extends WebPage> getHomePage() {
@@ -15,13 +12,7 @@ public class MonitorApplication extends WebApplication {
 
 	public void init() {
 		super.init();
-		uploadFolder = new Folder(System.getProperty("java.io.tmpdir"), "wicket-uploads");
-		uploadFolder.mkdirs();
 		getCspSettings().blocking().disabled();
-	}
-
-	public Folder getUploadFolder() {
-		return uploadFolder;
 	}
 
 }
